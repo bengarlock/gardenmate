e910f589fce146f187d808ef9f1cdc390b1df852
bgarlock <ben.garlock@gmail.com>
2024-02-17 09:24:07 -0500

    basic buttons built

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,17 @@ import PestManagement from "@/app/pest_management/page";
 
 export default function Home() {
 
-  return (
-      <div className={"flex flex-col items-center justify bg-amber-400"}>
-          <div className={"p-10 w-1/4"}>
-              <PestManagement/>
-          </div>
-      </div>
+    return (
+        <div className={"flex flex-col w-1/4"}>
+            <div className={"flex flex-col text-center bg-white w-full text-black border border-black shadow-lg " +
+                "mb-4 sm:mx-2 p-5"}>
+                Pest Management
+            </div>
+            <div className={"flex flex-col text-center bg-white w-full text-black border border-black shadow-lg " +
+                "mb-4 sm:mx-2 p-5"}>
+                Chickens
+            </div>
+        </div>
 
-  );
+    );
 }
--- src/app/pest_management/page.js
+++ src/app/pest_management/page.js
@@ -3,7 +3,10 @@
 const PestManagement = () => {
 
     return(
-        <div>Pest Management</div>
+        <div className={"flex flex-col text-center rounded-xl text-white border border-gray-700 hover:shadow-lg " +
+            "mb-4 sm:mx-2 p-5 w-full bg-gray-900"}>
+            Pest Management
+        </div>
     )
 }
 
