a893262b948e6b154109fa0a09fa50aa7020bf9d
bgarlock <ben.garlock@gmail.com>
2024-02-17 14:12:46 -0500

    working with graphs!

--- src/app/plants/page.js
+++ src/app/plants/page.js
@@ -1,10 +1,13 @@
 'use client'
 
+import LinePlot from "@/app/plants/linechart";
+
 const Plants = () => {
 
     return(
         <div className="w-full flex flex-row justify-center h-screen bg-gray-500 bg-opacity-90">
             Plants
+            <LinePlot />
         </div>
     )
 }
