56f3fbd3e3a5b23fbf4a19aa0e2925e9bc412551
bgarlock <ben.garlock@gmail.com>
2024-02-17 12:18:07 -0500

    navbar work

--- src/app/NavBar.js
+++ src/app/NavBar.js
@@ -1,21 +1,39 @@
 'use client'
+import Link from 'next/link';
+import {usePathname, useRouter} from 'next/navigation';
 
 
 const NavBar = () => {
 
+    const pathname = usePathname()
+    const router = useRouter();
+
+
     return (
-            <div className="w-1/6 flex flex-col items-center justify-center h-screen bg-gray-900 bg-opacity-75">
-                <div
-                    className={"w-full mb-5 p-3 text-white select-none hover:bg-amber-500 transition-colors duration-300 ease-in-out"}
-                >
+        <div className="w-1/6 min-w-44 flex flex-col justify-start h-screen bg-gray-900 bg-opacity-75">
+
+            <h1 className="w-full flex justify-center p-3 mb-11 text-amber-700">GardenMate</h1>
+
+            <Link href={"/"}>
+                <div className={"w-full mb-5 p-3 text-white select-none hover:bg-amber-500 transition-colors " +
+                    "duration-300 ease-in-out"}>
+                    Home
+                </div>
+            </Link>
+
+            <Link href={"/plants"}>
+                <div className={"w-full mb-5 p-3 text-white select-none hover:bg-amber-500 transition-colors " +
+                    "duration-300 ease-in-out"}>
                     Plants
                 </div>
-                <div
-                    className={"w-full mb-5 p-3 text-white select-none hover:bg-amber-500 transition-colors duration-300 ease-in-out"}
-                >
+            </Link>
+            <Link href={"/chickens"}>
+                <div className={"w-full mb-5 p-3 text-white select-none hover:bg-amber-500 transition-colors " +
+                    "duration-300 ease-in-out"}>
                     Chickens
                 </div>
-            </div>
+            </Link>
+        </div>
     )
 }
 
--- src/app/plants/page.js
+++ src/app/plants/page.js
@@ -1,13 +1,13 @@
 'use client'
 
-const PestManagement = () => {
+const Plants = () => {
 
     return(
         <div className={"flex flex-col text-center rounded-xl text-white border border-gray-700 hover:shadow-lg " +
             "mb-4 sm:mx-2 p-5 w-full bg-gray-900"}>
-            Pest Management
+            Plants
         </div>
     )
 }
 
-export default PestManagement
+export default Plants
