0b801d28b6915da8a4a8ff10f5efc3073dba677f
bgarlock <ben.garlock@gmail.com>
2024-02-17 12:00:21 -0500

    nav bar looking better

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,10 @@
-import PestManagement from "@/app/pest_management/page";
+import NavBar from "@/app/NavBar";
 
 export default function Home() {
 
     return (
-        <div className={"flex flex-col w-1/4"}>
-            <div className={"flex flex-col text-center bg-white w-full text-black border border-black shadow-lg " +
-                "mb-4 sm:mx-2 p-5"}>
-                Pest Management
-            </div>
-            <div className={"flex flex-col text-center bg-white w-full text-black border border-black shadow-lg " +
-                "mb-4 sm:mx-2 p-5"}>
-                Chickens
-            </div>
+        <div className={"flex flex-row w-full"}>
+            <NavBar />
         </div>
 
     );
