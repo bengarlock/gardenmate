d05dddf7ec321e5b6686350b5e2d129309456bec
bgarlock <ben.garlock@gmail.com>
2024-05-18 05:01:09 -0400

    adding tiles

---
+++ src/app/chickens/tile.js
@@ -0,0 +1,16 @@
+'use client'
+
+
+const Tile = (props) => {
+
+
+
+    return (
+        <div class="group m-2 w-48 h-48 overflow-hidden  shadow-lg transform transition-transform duration-300 hover:scale-105">
+            <img src="https://via.placeholder.com/150" alt="Placeholder Image" class="w-full h-full object-cover" />
+        </div>
+    )
+
+}
+
+export default Tile
