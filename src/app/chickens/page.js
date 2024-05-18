153821c277d654b9c011f6fc39ee6267a1f43d46
bgarlock <ben.garlock@gmail.com>
2024-05-18 05:01:09 -0400

    adding tiles

--- src/app/chickens/page.js
+++ src/app/chickens/page.js
@@ -1,10 +1,20 @@
 'use client'
 
+import Tile from "@/app/chickens/tile";
+
 const Chickens = () => {
 
     return (
-        <div className="w-full flex flex-row justify-center h-screen bg-gray-500 bg-opacity-90">
-            Chickens
+        <div className="w-full flex flex-col justify-center bg-gray-500 bg-opacity-90">
+            <h1>
+                Chickens
+            </h1>
+            <div className="w-full flex flex-row justify-evenly flex-wrap h-screen bg-gray-500 bg-opacity-90">
+                <Tile id={1} title="Nutrition" />
+                <Tile id={1} title="Nutrition" />
+                <Tile id={1} title="Nutrition" />
+                <Tile id={1} title="Nutrition" />
+            </div>
         </div>
     )
 }
