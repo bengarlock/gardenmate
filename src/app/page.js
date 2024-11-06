c9207093a635b67cf0dc6449c1f3e5aecc1cdfd0
bgarlock <ben.garlock@gmail.com>
2024-11-06 06:47:58 -0500

    weather

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,21 @@
 'use client';
 import {usePathname} from 'next/navigation';
+import Weather from "@/app/plants/weather/page";
 
 export default function Home() {
 
     return (
-        <div className="w-full flex flex-row justify-center h-screen bg-gray-500 bg-opacity-90">
-            Home
+        <div className="w-full flex flex-row flex-wrap justify-center h-screen bg-gray-500 bg-opacity-90">
+            <div className="group m-2 w-48 h-48 overflow-hidden rounded-xl bg-blue-900 bg-opacity-90 shadow-lg
+            transform transition-transform duration-300 hover:scale-105">
+                <div className="p-10">
+                    <h1>Weather</h1>
+                    <Weather/>
+                </div>
+            </div>
+
+
+
         </div>
 
     );
--- src/app/plants/weather/page.js
+++ src/app/plants/weather/page.js
@@ -1,9 +1,47 @@
 'use client'
 
+
+import {useEffect, useState} from "react";
+
 const Weather = () => {
 
-    return(
-        <div>Weather</div>
+    const [currentWeather, setCurrentWeather] = useState(null)
+
+    useEffect(() => {
+        fetchWeather()
+    }, []);
+
+
+    const fetchWeather = () => {
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        const raw = JSON.stringify({
+            "request": "get_weather_data"
+        });
+
+        const requestOptions = {
+            method: "POST",
+            headers: myHeaders,
+            body: raw,
+            redirect: "follow"
+        };
+
+        fetch("https://bengarlock.com/api/v1/garden/weather/", requestOptions)
+            .then((response) => response.json())
+            .then((result) => setCurrentWeather(result))
+            .catch((error) => console.error(error));
+    }
+
+    const renderWeather = () => {
+        if (currentWeather) {
+            const temp_f = (currentWeather.obs[0].air_temperature * 9/5) + 32
+            return Math.ceil(temp_f) + "\u00B0"
+        }
+    }
+
+    return (
+        <div>{renderWeather()} F</div>
     )
 }
 
