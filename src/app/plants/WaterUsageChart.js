9d1eb55692c2b4dd54a9630ff4c03873969c5ea1
Ben Garlock <ben.garlock@gmail.com>
2026-05-21 19:43:24 -0400

    Deploy gardenmate changes

--- src/app/plants/WaterUsageChart.js
+++ src/app/plants/WaterUsageChart.js
@@ -5,7 +5,7 @@ import {useEffect, useMemo, useRef, useState} from 'react'
 
 const WATER_USAGE_API = 'https://bengarlock.com/api/v1/garden/water/water-usage/'
 const WEEKDAY_LABELS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
-const WATER_COST_PER_GALLON = 0.59
+const WATER_COST_PER_GALLON = 0.0059
 const CURRENCY_FORMAT = new Intl.NumberFormat(undefined, {
     style: 'currency',
     currency: 'USD',
