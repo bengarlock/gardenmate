40030676fdd0bba8a984d7d723d6738da13042d6
Ben Garlock <ben.garlock@gmail.com>
2026-07-19 07:51:33 -0400

    Add Apple touch favicon metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,6 +7,16 @@ const inter = Inter({subsets: ["latin"]});
 export const metadata = {
     title: "GardenMate",
     description: "Tools to make things things grow",
+    icons: {
+        icon: "/favicon.ico",
+        apple: [
+            {
+                url: "/apple-touch-icon.png",
+                sizes: "180x180",
+                type: "image/png",
+            },
+        ],
+    },
 };
 
 export default function RootLayout({children}) {
